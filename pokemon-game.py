b0b878422e5c7a4646ea01c8bb6820bcb0f298ca
manu <manuelgutierrezchaves@gmail.com>
2022-06-15 21:10:50 +0200

    Added name validation (not repeated name) to pokemon class

--- pokemon-game.py
+++ pokemon-game.py
@@ -3,12 +3,23 @@ import random
 
 class pokemon():
 
+    names_used = []
+
     def __init__(self, name):
         self.name = name
         self.type = random.choice(["Water", "Fire", "Grass"])
         self.max_hp = 100
         self.hp = self.max_hp
         self.attack = 30
+        self.name_validation()
+
+
+    def name_validation(self):
+        if self.name not in self.names_used:
+            self.names_used.append(self.name)
+        else:
+            print("Name already in use")
+            del self
 
     def __str__(self):
         return self.name + ", Type: " + self.type + ", HP: " + str(round(self.hp))
