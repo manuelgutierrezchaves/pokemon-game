4b635664fb08682db36b92ad776aea36c95ceb8d
manu <manuelgutierrezchaves@gmail.com>
2022-06-15 20:39:06 +0200

    Added cure to pokemon class

--- pokemon-game.py
+++ pokemon-game.py
@@ -6,25 +6,24 @@ class pokemon():
     def __init__(self, name):
         self.name = name
         self.type = random.choice(["Water", "Fire", "Grass"])
-        self.hp = 100
+        self.max_hp = 100
+        self.hp = self.max_hp
         self.attack = 30
 
     def __str__(self):
         return self.name + ", Type: " + self.type + ", HP: " + str(round(self.hp))
 
     def evolution(self):
-        self.hp = self.hp * 1.6
+        self.max_hp = self.max_hp * 1.6
+        self.hp = self.max_hp
         self.attack = self.attack * 1.6
 
-    
-
-
-
+    def cure(self, cantidad):
+        self.hp = self.max_hp if self.hp + cantidad > self.max_hp else self.hp + cantidad           
 
 
 
 #-------------------Testing-----------------------
-poke1 = pokemon("Nidalee")
-print(poke1)
-poke1.evolution()
-print(poke1)
+poke = pokemon("Nidalee")
+poke.cure(100)
+print(poke)
